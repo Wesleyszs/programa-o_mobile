4d20afaa1fd2952c51ece6a9c24ef9d10c0b8ff0
wesley souza <122413661+Wesleyszs@users.noreply.github.com>
2024-08-26 10:45:47 -0300

    Update mobile.java
    cod funcionando

--- mobile.java
+++ mobile.java
@@ -1,10 +1,10 @@
-
 package com.example.aula1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,20 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void mudarTexto(View view){
+
+    public void SortearNumero(View view){
+        int numero=new Random().nextInt(11);
         TextView texto=findViewById(R.id.resultado);
-        texto.setText("Programando Mobile");
-        }
-
-    public static void SortearNumero(View view) {
-
-        Random random = new Random();
-        int numero = random.nextInt(11);
-
-
-        TextView texto = view.getRootView().findViewById(R.id.resultado);
-
-
-        texto.setText(String.valueOf(numero));
+        texto.setText("numero sorteado: "+ numero);
     }
 }
