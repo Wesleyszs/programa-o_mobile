efcf6339c24e047ebc486ca0ed9578f0d7da80b8
wesley souza <122413661+Wesleyszs@users.noreply.github.com>
2024-08-26 10:04:04 -0300

    Update mobile.java
    aula 2 , 1 comit , cod com erro

--- mobile.java
+++ mobile.java
@@ -1,1 +1,33 @@
 
+package com.example.aula1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mudarTexto(View view){
+        TextView texto=findViewById(R.id.resultado);
+        texto.setText("Programando Mobile");
+        }
+
+    public static void SortearNumero(View view) {
+
+        Random random = new Random();
+        int numero = random.nextInt(11);
+
+
+        TextView texto = view.getRootView().findViewById(R.id.resultado);
+
+
+        texto.setText(String.valueOf(numero));
+    }
+}
