2f46c37673bb2bc246632d76fd4eadec24a9487a
wesley souza <122413661+Wesleyszs@users.noreply.github.com>
2024-09-07 20:42:01 -0300

    Create DatabaseHelper.java

---
+++ LOGIN/DatabaseHelper.java
@@ -0,0 +1,39 @@
+package com.example.loginapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "app.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Nome da tabela e colunas
+    private static final String TABLE_USERS = "users";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_PASSWORD = "password";
+
+    // SQL para criar a tabela
+    private static final String TABLE_CREATE =
+        "CREATE TABLE " + TABLE_USERS + " (" +
+        COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        COLUMN_EMAIL + " TEXT UNIQUE NOT NULL, " +
+        COLUMN_PASSWORD + " TEXT NOT NULL);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+}
