5e7fe7940834fe4869034ea95a306b27ad27ddd9
wesley souza <122413661+Wesleyszs@users.noreply.github.com>
2024-09-07 20:42:54 -0300

    Create HomeActivity.java

---
+++ LOGIN/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.loginapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home); // Certifique-se de ter o layout correspondente
+    }
+}
